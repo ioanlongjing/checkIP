5aa0d5ca61f6e46f198969c98a6f19de437f9346
ioanlongjing <ioan.snickersnee@gmail.com>
2017-05-19 13:20:26 +0800

    init file

---
+++ a.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IPFile=host1.txt
+echo "Do you want use host1.txt?"
+
+
+cat $IPFile | while read line
+do
+	ping -c1 -W1 $line 
+	if [ "$?" -eq "0" ]; then
+		echo "$line is up"
+	else
+		echo "$line" >> failIP.txt
+		echo "============================"
+		# To IP to fail.
+		echo "This IP was down"
+		cat failIP.txt 
+	fi
+done
